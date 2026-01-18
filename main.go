b64cd8a51abea4937254ef5873dd932d7afe9781
Ankur Pandey <pankur216@gmail.com>
2026-01-17 19:43:30 -0500

    (feat): add skeleton functions for two routes
    First route is /snippet/view, which will be used to view the snippet and the other is /snippet/create, which will be used to create a snippet. Everything is a skeleton right now i.e. doesn't have any useful logic

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a specific snippet"))
+}
+
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
+
+	// routes
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass
 	// two parameters: the TCP network address to listen on (in this case ":4000
